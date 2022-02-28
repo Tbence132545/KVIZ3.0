91605492de15185b7a7d8bf8d74144a0d4e26869
Tbence132545 <tbence2005@gmail.com>
2022-02-28 23:26:19 +0100

    Kerdes osztály

--- KVIZ3.0/Kviz.xaml.cs
+++ KVIZ3.0/Kviz.xaml.cs
@@ -17,12 +17,17 @@ namespace KVIZ3._0
     /// </summary>
     public partial class Kviz : Window
     {
-        public Kviz()
+      
+        public Kviz(string fajlnev)
         {
             InitializeComponent();
         }
 
         private void result_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void LoadQuestion()
         {
 
         }
@@ -48,5 +53,29 @@ namespace KVIZ3._0
         }
 
        
+    }
+    class Kerdes
+    {
+        List<string> answers = new List<string>();
+        int correctAnswer;
+        string kerdes;
+        int selected = -1;
+        public Kerdes(string sor)
+        {
+
+            string[] splitted = sor.Split(';');
+            kerdes = splitted[0];
+            for (int i = 1; i < splitted.Length - 1; i++)
+            {
+
+                answers.Add(splitted[i]);
+            }
+            correctAnswer = Convert.ToInt32(splitted[5]);
+
+        }
+        public string akerdes { get => kerdes; }
+        public List<string> valaszok { get => answers; }
+        public int kivalasztott { get => selected; set => selected = value; }
+        public int correctindex { get => correctAnswer; }
     }
 }
