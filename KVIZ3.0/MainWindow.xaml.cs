889ea4e82116ffbd9097f151f8e06d8069444451
Tbence132545 <tbence2005@gmail.com>
2022-02-28 23:21:56 +0100

    Basic layout

--- KVIZ3.0/MainWindow.xaml.cs
+++ KVIZ3.0/MainWindow.xaml.cs
@@ -12,17 +12,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace KVIZ3._0
 {
+   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        static string fajlnev;
+        static List<string> temak;
         public MainWindow()
         {
             InitializeComponent();
+            temak = new List<string>();
+            using (StreamReader sr = new StreamReader("temak.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    temak.Add(sr.ReadLine());
+                }
+
+            }
+            temavalaszto.ItemsSource = temak;
+        }
+        
+
+        private void start_btn_Click(object sender,RoutedEventArgs e) 
+        { 
+        }
+        private void temakivalasztva(object sender, SelectionChangedEventArgs e)
+        {
+            start_btn.IsEnabled = true;
+            fajlnev = (string)temavalaszto.SelectedItem;
         }
     }
 }
