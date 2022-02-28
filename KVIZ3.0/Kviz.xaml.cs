87c7d701a76caccbe1c8dee080527e74cb71d2b4
Tbence132545 <tbence2005@gmail.com>
2022-02-28 23:24:12 +0100

    Kviz ablak

---
+++ KVIZ3.0/Kviz.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KVIZ3._0
+{
+    /// <summary>
+    /// Interaction logic for Kviz.xaml
+    /// </summary>
+    public partial class Kviz : Window
+    {
+        public Kviz()
+        {
+            InitializeComponent();
+        }
+
+        private void result_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void option_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void previous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fomenu_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
